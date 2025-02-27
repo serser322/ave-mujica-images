b79752afffe6a8907698b33287270b9e3b95a69f
Wei Chiao Chen <www322.joe@gmail.com>
2025-02-28 00:37:19 +0800

    feat: Add sort button condition

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,4 +1,6 @@
 import { Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
 import SearchBar from '@/components/SearchBar';
 import ImageList from '@/components/ImageList';
 import ToTopButton from '@/components/ToTopButton';
@@ -7,6 +9,7 @@ import '@/styles/HomePage.scss';
 import { SortToggleButtons } from '@/components/SortToggleButtons';
 
 export default function HomePage() {
+  const episode = useSelector((state: RootState) => state.contentLayout.episode);
   return (
     <>
       <Box className="search-area come-in-animation">
@@ -16,9 +19,12 @@ export default function HomePage() {
         <Box sx={{ flex: 1 }}>
           <RangeSelectBar />
         </Box>
-        <Box sx={{ flex: 0.5 }}>
-          <SortToggleButtons />
-        </Box>
+
+        {episode === 0 && (
+          <Box sx={{ flex: 0.5 }}>
+            <SortToggleButtons />
+          </Box>
+        )}
       </Box>
       <ImageList />
       <Box>
