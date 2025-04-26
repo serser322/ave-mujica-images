2308fca8d82df5b20618a23cac1a1991e657224c
Wei Chiao Chen <www322.joe@gmail.com>
2025-04-26 21:18:01 +0800

    feat: Update new header background, logo and web info

--- src/layout/HeaderLayout.tsx
+++ src/layout/HeaderLayout.tsx
@@ -9,16 +9,16 @@ export default function HeaderLayout() {
     <>
       <header>
         <Box className="logo-wrapper">
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Box sx={{ mr: 2.6, mb: 0.6, width: '50px', color: '#ffffff' }}>
-              <ImageSearch sx={{ fontSize: { xs: 55, md: 65 } }} />
+          <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+            <Box sx={{ mr: 2.6, width: '50px', color: '#ffffff' }}>
+              <ImageSearch sx={{ fontSize: { xs: 50, md: 60 } }} />
             </Box>
             <Box className="logo">
               <img src={logoUrl} alt="logo" />
             </Box>
           </Box>
           <Box sx={{ pl: 0.5, fontSize: 14, fontWeight: 600, color: '#e0e0e0' }}>
-            就將一切<span className="line-through">對話訊息</span>委身於Ave Mujica吧！
+            就將一切<span className="line-through">對話訊息</span>委身於 MyGo 與 Ave Mujica 吧！
           </Box>
         </Box>
         {/* <Box className="update-info">最新集數: 第{latestEpisode}集</Box> */}
