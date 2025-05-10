6c9f56d592060d7934da2fda8fa59f37f478901a
Wei Chiao Chen <www322.joe@gmail.com>
2025-05-10 22:21:34 +0800

    feat: add mygo 7th episode images

--- src/components/images/ImageList.tsx
+++ src/components/images/ImageList.tsx
@@ -123,7 +123,9 @@ export default function ImageList() {
               <Box sx={{ width: '36px', mr: 3 }}>
                 <img src={underConstructionImg} alt="" style={{ width: '100%' }} />
               </Box>
-              <Box sx={{ fontSize: '0.9rem', color: '#e6e6e6' }}>MyGO 目前更新至第6集，持續施工中...</Box>
+              <Box sx={{ fontSize: '0.9rem', color: '#e6e6e6' }}>
+                MyGO 目前更新至第{myGOImages.slice(-1)[0].episode}集，持續施工中...
+              </Box>
             </Box>
           )}
           <Box sx={{ textAlign: 'center' }}>
